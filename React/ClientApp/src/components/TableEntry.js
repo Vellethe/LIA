1476c2a8729249a849073982e90426a0b7fc9a60
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-10-25 09:16:23 +0200

    better css for table

--- React/ClientApp/src/components/TableEntry.js
+++ React/ClientApp/src/components/TableEntry.js
@@ -5,12 +5,13 @@ import React from "react";
 export const TableEntry = () => {
     return (
         <tr className="row2">
-                <td className="text">Name</td>
-                <td className="text">Role</td>
-                <td className="text">Contact name</td>
-                <td className="text">date</td>
-                <td className="text">Location</td>
-                <td className="text">tags</td>
+            <td className="text">Name</td>
+            <td className="text">Role</td>
+            <td className="text">Contact name</td>
+            <td className="text">date</td>
+            <td className="text">Location</td>
+            <td className="text">tags</td>
+            <td className="text"><input type="checkbox"></input></td>
         </tr>
     )
 }
