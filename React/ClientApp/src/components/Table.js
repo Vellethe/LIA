60d368f4e93d0d43d0e7b3c41e3c0dd95be839b6
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-10-25 09:50:16 +0200

    added feilds to tableEntry

--- React/ClientApp/src/components/Table.js
+++ React/ClientApp/src/components/Table.js
@@ -1,28 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Table.css";
 import { TableEntry } from "./TableEntry"
 
-export const Table2 = () => {
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th className="name">Name</th>
-                    <th className="text">Role</th>
-                    <th className="text">Contact name</th>
-                    <th className="text">Date</th>
-                    <th className="text">Location</th>
-                    <th className="text">Tags</th>
-                    <th className="text">Favorites</th>
-                </tr>
-            </thead>
-            <tbody>
-                <TableEntry/>
-                <TableEntry/>
-                <TableEntry/>
-            </tbody>
-        </table>
-    )
+export class Table2 extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                { name: "batman", role: "Hero", contactName: "Bruce Wane", date: "2023-10-25", location: "Gotham City", tags: ["crime", "hero", "stuff"], favorit: true },
+                { name: "joker", role: "Vilan", contactName: " The rideler", date: "2023-10-25", location: "Gotham City", tags: ["crime", "hero", "stuff"], favorit: true }
+            ]
+        };
+    }
+
+    static renderTable(data) {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th className="name">Name</th>
+                        <th className="text">Role</th>
+                        <th className="text">Contact name</th>
+                        <th className="text">Date</th>
+                        <th className="text">Location</th>
+                        <th className="text">Tags</th>
+                        <th className="text">Favorites</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(job =>
+                        <TableEntry job={job} />
+                    )}
+                </tbody>
+            </table>
+        )
+    }
+
+    render() {
+        return Table2.renderTable(this.state.data);
+    }
 }
 
 export const Table = () => {
--- React/ClientApp/src/components/TableEntry.js
+++ React/ClientApp/src/components/TableEntry.js
@@ -2,15 +2,15 @@ import PropTypes from "prop-types";
 import React from "react";
 //import "./style.css";
 
-export const TableEntry = () => {
+export function TableEntry({job}){
     return (
         <tr className="row2">
-            <td className="text">Name</td>
-            <td className="text">Role</td>
-            <td className="text">Contact name</td>
-            <td className="text">date</td>
-            <td className="text">Location</td>
-            <td className="text">tags</td>
+            <td className="text">{ job.name}</td>
+            <td className="text">{job.role}</td>
+            <td className="text">{job.contactName}</td>
+            <td className="text">{job.date}</td>
+            <td className="text">{job.location}</td>
+            <td className="text">{job.tags}</td>
             <td className="text"><input type="checkbox"></input></td>
         </tr>
     )
