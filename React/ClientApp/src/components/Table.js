31e88a9df1caae053e0f6e573b5e1565c0b1d66a
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-10-26 09:24:50 +0200

    basic sort implemented

--- React/ClientApp/src/components/Table.js
+++ React/ClientApp/src/components/Table.js
@@ -9,14 +9,26 @@ export class Table extends Component{
         this.state = {
             data: [
                 { id:1,name: "batman", role: "Hero", contactName: "Bruce Wane", date: "2023-10-25", location: "Gotham City", tags: ["crime", "hero", "stuff"], favorit: true },
-                { id:2,name: "joker", role: "Vilan", contactName: " The rideler", date: "2023-10-25", location: "Gotham City", tags: ["crime", "hero", "stuff"], favorit: true }
+                { id:2,name: "joker", role: "Vilan", contactName: " The rideler", date: "2023-10-26", location: "Gotham City", tags: ["crime", "hero", "stuff"], favorit: true },
+                { id:3,name: "the rideler", role: "vilan", contactName: "Bruce Wane", date: "2022-10-25", location: "Gotham City", tags: ["crime", "hero", "stuff"], favorit: true },
             ]
         };
+        this.sortDate = this.sortDate.bind(this);
     }
 
-    static renderTable(data) {
-        return (
+    sortDate(useDecendingOrder) {
+        var curentData = this.state.data;
+        var newData = curentData.sort(function (a, b) {
+            return new Date(b.date) - new Date(a.date);
+        });
+        this.setState({ data: newData })
+    }
+
+    render() {
+        return (<div>
+
             <table>
+                <button onClick={() => this.sortDate(true)}>flase</button>
                 <thead>
                     <tr>
                         <th className="name">Name</th>
@@ -29,16 +41,12 @@ export class Table extends Component{
                     </tr>
                 </thead>
                 <tbody>
-                    {data.map(job =>
-                        <TableEntry key={job.id } job={job} />
+                    {this.state.data.map(job =>
+                        <TableEntry key={job.id} job={job} />
                     )}
                 </tbody>
             </table>
-        )
-    }
-
-    render() {
-        return Table.renderTable(this.state.data);
+        </div>)
     }
 }
 
