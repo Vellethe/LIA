a507bdc832020a996b709cb00a553b46486bb762
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-10-26 15:31:22 +0200

    .

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,5 +1,6 @@
 using Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace Api
 {
@@ -18,7 +19,10 @@ namespace Api
 
             builder.Services.AddDbContext<JobScoutContext>(options =>
             {
-                options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=JobScout;Integrated Security=True;MultipleActiveResultSets=true"); //TODO add string
+                //options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=JobScout;Integrated Security=True;MultipleActiveResultSets=true"); //TODO add string
+
+                //Allan
+                options.UseSqlServer(@"Server=MSI\SQLEXPRESS;Database=Jobscout;Integrated Security=true;TrustServerCertificate=true;");
             });
 
             var app = builder.Build();
