46c2d847a273cb83e3929b0cbe339e7279b6f9d5
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-10-27 14:01:49 +0200

    .

--- React/ClientApp/src/components/Table.js
+++ React/ClientApp/src/components/Table.js
@@ -47,11 +47,10 @@ export const Table = () => {
                 <tr>
                     <th onClick={() => sortName(isAscending)} className={"name " + (sortColumn === "name" ? (isAscending ? "ascending":"descending"):"")}>Company</th>
                     <th className="text">Role</th>
-                    <th className="text">Contact name</th>
-                    <th className="text">Date</th>
                     <th className="text">Location</th>
-                    <th className="text">Tags</th>
                     <th className="text">Favorites</th>
+                    <th className="text">Excluded</th>
+                    <th className="text">Date</th>
                 </tr>
             </thead>
             <tbody>
