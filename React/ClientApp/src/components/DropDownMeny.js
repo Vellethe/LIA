0837372ddc1462eebc35ec9a62d922ecb8129194
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-10-27 10:14:37 +0200

    Dropdown menu checkbox+css

--- React/ClientApp/src/components/DropDownMeny.js
+++ React/ClientApp/src/components/DropDownMeny.js
@@ -12,17 +12,46 @@ function Dropdown() {
         <div className="dropdown">
             <button onClick={toggleDropdown} className="dropdown-button">Skills</button>
             {isOpen && (
+                <div className="scrollMenu">
                 <ul className="dropdown-list">
-                    <li>Developer</li>
-                    <li>Software</li>
-                    <li>Fullstack</li>
-                    <li>Embedded</li>
-                    <li>Backend</li>
-                    <li>Frontend</li>
-                    <li>Tester</li>
-                    <li>Python</li>
-                    <li>C#</li>
-                </ul>
+                        <li>
+                            <input type="checkbox" id="developer" />
+                            <label htmlFor="developer">Developer</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="software" />
+                            <label htmlFor="software">Software</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="fullstack" />
+                            <label htmlFor="fullstack">Fullstack</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="embedded" />
+                            <label htmlFor="embedded">Embedded</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="backend" />
+                            <label htmlFor="backend">Backend</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="frontend" />
+                            <label htmlFor="frontend">Frontend</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="tester" />
+                            <label htmlFor="tester">Tester</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="python" />
+                            <label htmlFor="python">Python</label>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="csharp" />
+                            <label htmlFor="csharp">C#</label>
+                        </li>
+                    </ul>
+                </div>
             )}
         </div>
     );
