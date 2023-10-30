e2282e7dd8885eecb93912633488c119e4e54c7f
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-10-30 10:58:13 +0100

    Minutes code but not fully working

--- React/ClientApp/src/components/Table.js
+++ React/ClientApp/src/components/Table.js
@@ -6,6 +6,8 @@ var dataArray = [
     { id: 1, name: "batman", role: "Hero", contactName: "Bruce Wane", date: "2023-10-25", location: "Gotham City", tags: ["crime", "hero", "stuff"], favorit: true },
     { id: 2, name: "joker", role: "Vilan", contactName: " The rideler", date: "2023-10-26", location: "Gotham City", tags: ["crime", "hero", "stuff"], favorit: true },
     { id: 3, name: "the rideler", role: "vilan", contactName: "Bruce Wane", date: "2022-10-25", location: "Gotham City", tags: ["crime", "hero", "stuff"], favorit: true },
+    { id: 4, name: "Company A", role: "Cookiemonster", contactName: "Blue Creature", date: "2023-10-28 10:30:00", location: "Narnia", tags: ["friendly", "blue", "big"], favorit: true },
+    { id: 5, name: "Company B", role: "Monstercookie", contactName: "Creature Blue", date: "2023-10-28 09:30:00", location: "Narnia", tags: ["friendly", "blue", "big"], favorit: true }
 ]
 
 
@@ -14,20 +16,45 @@ export const Table = () => {
     const [isAscending, setAscending] = useState(true);
     const [sortColumn, setSortColumn] = useState("date");
 
-    const sortDate = (useAscendingOrder) => {
-        setSortColumn("date")
+    const getMinutesAgo = (date) => {
+        const now = new Date();
+        const postDate = new Date(date);
+        const diff = Math.floor((now - postDate) / (1000 * 60)); // Convert milliseconds to minutes
+        return diff;
+    }
+
+    //const sortDate = (useAscendingOrder) => {
+    //    setSortColumn("date")
+    //    setAscending(!isAscending);
+    //    const newData = [...jobData];
+    //    newData.sort((a, b) => {
+    //        if (isAscending) {
+    //            return new Date(a.date) - new Date(b.date);
+    //        }
+    //        else {
+    //            return new Date(b.date) - new Date(a.date);
+    //        }
+    //    });
+    //    setJobData(newData);
+    //}
+
+    const sortDate = () => {
+        setSortColumn("date");
         setAscending(!isAscending);
         const newData = [...jobData];
         newData.sort((a, b) => {
+            const diffA = getMinutesAgo(a.date);
+            const diffB = getMinutesAgo(b.date);
+
             if (isAscending) {
-                return new Date(a.date) - new Date(b.date);
-            }
-            else {
-                return new Date(b.date) - new Date(a.date);
+                return diffA - diffB;
+            } else {
+                return diffB - diffA;
             }
         });
         setJobData(newData);
     }
+
     const sortName = () => {
         setSortColumn("date")
         setAscending(!isAscending);
@@ -65,56 +92,3 @@ export const Table = () => {
         </table>
     </div>)
 }
-//export const Table = () => {
-//    const [jobData, setJobData] = useState(dataArray);
-//    const [isAscending, setAscending] = useState(true);
-//    const [sortColumn, setSortColumn] = useState("date");
-
-//    const sortDate = (useAscendingOrder) => {
-//        setSortColumn("date")
-//        setAscending(!isAscending);
-//        var curentData = jobData;
-//        var newData = curentData.sort(function (a, b) {
-//            if (useAscendingOrder) {
-//                var x = a;
-//                a = b;
-//                b = x;
-//            }
-//            return new Date(b.date) - new Date(a.date);
-//        });
-//        setJobData([...newData]);
-//    }
-//    const sortName = (useAscendingOrder) => {
-//        setSortColumn("name")
-//        setAscending(!isAscending);
-//        var newData = jobData.sort(function (a, b) {
-//            return a.name.localeCompare(b.name, undefined, { sensitivity: "base" });
-//        })
-//        if (useAscendingOrder) {
-//            newData.reverse()
-//        }
-//        setJobData([...newData]);
-//    }
-//    return (<div>
-
-//        <button onClick={() => sortDate(isAscending)}>false</button>
-//        <table>
-//            <thead>
-//                <tr>
-//                    <th onClick={() => sortName(isAscending)} className={"name " + (sortColumn === "name" ? (isAscending ? "ascending" : "descending") : "")}>Company</th>
-//                    <th></th>
-//                    <th className="text">Favorites</th>
-//                    <th className="text">Excluded</th>
-//                    <th className="text">Date</th>
-//                </tr>
-//            </thead>
-//            <tbody>
-//                {jobData.map(job =>
-//                    <TableEntry key={job.id} job={job} />
-//                )}
-//            </tbody>
-//        </table>
-//    </div>)
-//}
-
-
