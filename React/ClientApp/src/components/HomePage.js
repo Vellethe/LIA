66f9e67f7db51dd6b4262d31cc3968e2d726a7da
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-10-30 12:18:29 +0100

    Parsing error homepage

--- React/ClientApp/src/components/HomePage.js
+++ React/ClientApp/src/components/HomePage.js
@@ -5,37 +5,65 @@ import "./HomePage.css";
 import { Table } from './Table';
 import Dropdown from './DropDown';
 
-export const HomePage = () => {
-    return (
-        <div>
-            <div id="homeSearchFields">
-                <form>
-                    <input
-                        id="homeSearchJobs"
-                        type="text"
-                        name="searchJobs"
-                        placeholder="Search for title or keywords" />
-                </form>
-                <form>
-                    <input
-                        id="homeSearchLocation"
-                        type="text"
-                        name="searchLocation"
-                        placeholder="Location" />
-                        <button id="homeSearchButton" type="submit">Search</button>
-                </form>
-            </div>
-            <div id="dates">
-                <input type="date" />
-                <input type="date" />
-            </div>
-            <div>
-                <Dropdown />
-            </div>
-            <div>
-                <Table />
-            </div>
 
-        </div>
-    );
-};
+export const HomePage = () => {
+
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+
+    const handleStartDateChange = (e) => {
+        setStartDate(e.target.value);
+    };
+
+    const handleEndDateChange = (e) => {
+        setEndDate(e.target.value);
+    };
+    const filterDataByDate = () => {
+        const filteredData = dataArray.filter((job) => {
+            const jobDate = new Date(job.date);
+            const filterStartDate = new Date(startDate);
+            const filterEndDate = new Date(endDate);
+            return jobDate >= filterStartDate && jobDate <= filterEndDate;
+        });
+
+        return (
+            <div>
+                <div id="homeSearchFields">
+                    <form>
+                        <input
+                            id="homeSearchJobs"
+                            type="text"
+                            name="searchJobs"
+                            placeholder="Search for title or keywords" />
+                    </form>
+                    <form>
+                        <input
+                            id="homeSearchLocation"
+                            type="text"
+                            name="searchLocation"
+                            placeholder="Location" />
+                        <button id="homeSearchButton" type="submit">Search</button>
+                    </form>
+                </div>
+                <div id="dates">
+                    <input
+                        type="date"
+                        value={startDate}
+                        onChange={handleStartDateChange}
+                    />
+                    <input
+                        type="date"
+                        value={endDate}
+                        onChange={handleEndDateChange}
+                    />
+                    <button onClick={filterDataByDate}>Date filter</button>
+                </div>
+                <div>
+                    <Dropdown />
+                </div>
+                <div>
+                    <Table />
+                </div>
+            </div>
+        );
+    };
