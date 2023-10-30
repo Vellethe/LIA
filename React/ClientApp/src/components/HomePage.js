3096b6244d97f6163f1de6e21c2c39cf578b5592
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-10-30 13:43:59 +0100

    Datefilter not working

--- React/ClientApp/src/components/HomePage.js
+++ React/ClientApp/src/components/HomePage.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-no-undef */
-import React from 'react';
+import React, { useState } from 'react';
 //import React from "react";
 import "./HomePage.css";
 import { Table } from './Table';
@@ -18,52 +18,55 @@ export const HomePage = () => {
     const handleEndDateChange = (e) => {
         setEndDate(e.target.value);
     };
+    const dataArray = [];
     const filterDataByDate = () => {
-        const filteredData = dataArray.filter((job) => {
+
+        dataArray.filter((job) => {
             const jobDate = new Date(job.date);
             const filterStartDate = new Date(startDate);
             const filterEndDate = new Date(endDate);
             return jobDate >= filterStartDate && jobDate <= filterEndDate;
         });
-
-        return (
-            <div>
-                <div id="homeSearchFields">
-                    <form>
-                        <input
-                            id="homeSearchJobs"
-                            type="text"
-                            name="searchJobs"
-                            placeholder="Search for title or keywords" />
-                    </form>
-                    <form>
-                        <input
-                            id="homeSearchLocation"
-                            type="text"
-                            name="searchLocation"
-                            placeholder="Location" />
-                        <button id="homeSearchButton" type="submit">Search</button>
-                    </form>
-                </div>
-                <div id="dates">
-                    <input
-                        type="date"
-                        value={startDate}
-                        onChange={handleStartDateChange}
-                    />
-                    <input
-                        type="date"
-                        value={endDate}
-                        onChange={handleEndDateChange}
-                    />
-                    <button onClick={filterDataByDate}>Date filter</button>
-                </div>
-                <div>
-                    <Dropdown />
-                </div>
-                <div>
-                    <Table />
-                </div>
-            </div>
-        );
     };
+
+    return (
+        <div>
+            <div id="homeSearchFields">
+                <form>
+                    <input
+                        id="homeSearchJobs"
+                        type="text"
+                        name="searchJobs"
+                        placeholder="Search for title or keywords" />
+                </form>
+                <form>
+                    <input
+                        id="homeSearchLocation"
+                        type="text"
+                        name="searchLocation"
+                        placeholder="Location" />
+                    <button id="homeSearchButton" type="submit">Search</button>
+                </form>
+            </div>
+            <div id="dates">
+                <input
+                    type="date"
+                    value={startDate}
+                    onChange={handleStartDateChange}
+                />
+                <input
+                    type="date"
+                    value={endDate}
+                    onChange={handleEndDateChange}
+                />
+                <button onClick={filterDataByDate}>Date filter</button>
+            </div>
+            <div>
+                <Dropdown />
+            </div>
+            <div>
+                <Table />
+            </div>
+        </div>
+    );
+};
