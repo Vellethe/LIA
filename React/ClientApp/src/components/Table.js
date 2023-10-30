78c8c7389aa195e9a56222f0d0b4723e9d651dcc
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-10-30 10:24:09 +0100

    SortingByDate

--- React/ClientApp/src/components/Table.js
+++ React/ClientApp/src/components/Table.js
@@ -17,27 +17,30 @@ export const Table = () => {
     const sortDate = (useAscendingOrder) => {
         setSortColumn("date")
         setAscending(!isAscending);
-        var curentData = jobData;
-        var newData = curentData.sort(function (a, b) {
-            if (useAscendingOrder) {
-                var x = a;
-                a = b;
-                b = x;
+        const newData = [...jobData];
+        newData.sort((a, b) => {
+            if (isAscending) {
+                return new Date(a.date) - new Date(b.date);
             }
-            return new Date(b.date) - new Date(a.date);
+            else {
+                return new Date(b.date) - new Date(a.date);
+            }
         });
-        setJobData([...newData]);
+        setJobData(newData);
     }
-    const sortName = (useAscendingOrder) => {
-        setSortColumn("name")
+    const sortName = () => {
+        setSortColumn("date")
         setAscending(!isAscending);
-        var newData = jobData.sort(function (a, b) {
-            return a.name.localeCompare(b.name, undefined, {sensitivity: "base"});
-        })
-        if (useAscendingOrder) {
-            newData.reverse()
-        }
-        setJobData([...newData]);
+        const newData = [...jobData];
+        newData.sort((a, b) => {
+            if (isAscending) {
+                return a.date.localeCompare(b.date, undefined, { sensitivity: "base" });
+            }
+            else {
+                return b.date.localeCompare(a.date, undefined, { sensitivity: "base" });
+            }
+        });
+        setJobData(newData);
     }
     return (<div>
 
@@ -45,11 +48,11 @@ export const Table = () => {
         <table>
             <thead>
                 <tr>
-                    <th onClick={() => sortName(isAscending)} className={"name " + (sortColumn === "name" ? (isAscending ? "ascending":"descending"):"")}>Company</th>
+                    <th className="text">Company</th>
                     <th></th>
                     <th className="text">Favorites</th>
                     <th className="text">Excluded</th>
-                    <th className="text">Date</th>
+                    <th onClick={() => sortName(isAscending)} className={"name " + (sortColumn === "name" ? (isAscending ? "ascending" : "descending") : "")}>Date</th>
                 </tr>
             </thead>
             <tbody>
@@ -60,4 +63,56 @@ export const Table = () => {
         </table>
     </div>)
 }
+//export const Table = () => {
+//    const [jobData, setJobData] = useState(dataArray);
+//    const [isAscending, setAscending] = useState(true);
+//    const [sortColumn, setSortColumn] = useState("date");
+
+//    const sortDate = (useAscendingOrder) => {
+//        setSortColumn("date")
+//        setAscending(!isAscending);
+//        var curentData = jobData;
+//        var newData = curentData.sort(function (a, b) {
+//            if (useAscendingOrder) {
+//                var x = a;
+//                a = b;
+//                b = x;
+//            }
+//            return new Date(b.date) - new Date(a.date);
+//        });
+//        setJobData([...newData]);
+//    }
+//    const sortName = (useAscendingOrder) => {
+//        setSortColumn("name")
+//        setAscending(!isAscending);
+//        var newData = jobData.sort(function (a, b) {
+//            return a.name.localeCompare(b.name, undefined, { sensitivity: "base" });
+//        })
+//        if (useAscendingOrder) {
+//            newData.reverse()
+//        }
+//        setJobData([...newData]);
+//    }
+//    return (<div>
+
+//        <button onClick={() => sortDate(isAscending)}>false</button>
+//        <table>
+//            <thead>
+//                <tr>
+//                    <th onClick={() => sortName(isAscending)} className={"name " + (sortColumn === "name" ? (isAscending ? "ascending" : "descending") : "")}>Company</th>
+//                    <th></th>
+//                    <th className="text">Favorites</th>
+//                    <th className="text">Excluded</th>
+//                    <th className="text">Date</th>
+//                </tr>
+//            </thead>
+//            <tbody>
+//                {jobData.map(job =>
+//                    <TableEntry key={job.id} job={job} />
+//                )}
+//            </tbody>
+//        </table>
+//    </div>)
+//}
+
 
