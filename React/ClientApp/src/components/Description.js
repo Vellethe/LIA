f1ca9cbc23284a7667d23ac9bd1a93b2ac0890dc
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-11-01 13:53:32 +0100

    Slice dataArray for better testing

--- React/ClientApp/src/components/Description.js
+++ React/ClientApp/src/components/Description.js
@@ -17,31 +17,39 @@ export const DescriptionPage = () => {
     return (
         <div className={styles['container']}>
             <div className={styles['body']}>
-                {dataArray.map(job => (
-                    <div className={styles.grid} key={job.id}>
-                        <div className={styles.row}>
-                            <div className={styles.label}>Name</div>
-                            <div className={styles.value}>{job.name}</div>
+                {dataArray.slice(0, 1).map(job => (
+                    <div key={job.id}>
+                        <div className={styles.grid}>
+                            <div className={styles.row}>
+                                <div className={styles.label}>Name:</div>
+                                <div className={styles.value}>{job.name}</div>
+                            </div>
+                            <div className={styles.row}>
+                                <div className={styles.label}>Location:</div>
+                                <div className={styles.value}>{job.location}</div>
+                            </div>
                         </div>
-                        <div className={styles.row}>
-                            <div className={styles.label}>Location</div>
-                            <div className={styles.value}>{job.location}</div>
+
+                        <div className={styles.grid}>
+                            <div className={styles.row}>
+                                <div className={styles.label}>Date:</div>
+                                <div className={styles.value}>{job.date}</div>
+                            </div>
+                            <div className={styles.row}>
+                                <div className={styles.label}>Role:</div>
+                                <div className={styles.value}>{job.role}</div>
+                            </div>
                         </div>
-                        <div className={styles.row}>
-                            <div className={styles.label}>Date</div>
-                            <div className={styles.value}>{job.date}</div>
-                        </div>
-                        <div className={styles.row}>
-                            <div className={styles.label}>Role</div>
-                            <div className={styles.value}>{job.role}</div>
-                        </div>
-                        <div className={styles.row}>
-                            <div className={styles.label}>Tags</div>
-                            <div className={styles.value}>{parseTags(job.tags)}</div>
-                        </div>
-                        <div className={styles.row}>
-                            <div className={styles.label}>Contacts</div>
-                            <div className={styles.value}>{job.contactName}</div>
+
+                        <div className={styles.grid}>
+                            <div className={styles.row}>
+                                <div className={styles.label}>Tags:</div>
+                                <div className={styles.value}>{parseTags(job.tags)}</div>
+                            </div>
+                            <div className={styles.row}>
+                                <div className={styles.label}>Contacts:</div>
+                                <div className={styles.value}>{job.contactName}</div>
+                            </div>
                         </div>
                     </div>
                 ))}
