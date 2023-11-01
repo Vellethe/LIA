1c6d750938e37edba90e3d75e7979b588bcaf85f
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-11-01 13:04:02 +0100

    desc works

--- React/ClientApp/src/components/Description.js
+++ React/ClientApp/src/components/Description.js
@@ -1,31 +1,31 @@
-//import React from 'react';
-//import { dataArray } from './JobData'
-//import styles from './Description.module.css';
+import React from 'react';
+import { dataArray } from './JobData'
+import styles from './Description.module.css';
 
-//export const DescriptionPage = () => {
-//    return (
-//        <div className={styles['grid-container']}>
-//            <div className={styles['grid-header']}>
-//                <div className={styles['grid-header-cell']}>{}Company</div>
-//                <div className={styles['grid-header-cell']}>{}Favorites</div>
-//                <div className={styles['grid-header-cell']}>{}Excluded</div>
-//                <div className={styles['grid-header-cell']}>Date</div>
-//            </div>
-//            <div className={styles['grid-body']}>
-//                {dataArray.map(job => (
-//                    <div className={styles.grid} key={job.id}>
-//                        <div className={styles.row1}>
-//                            <div className={styles.textName}>{job.name}</div>
-//                        </div>
-//                        <div className={styles.row2}>
-//                            <div className={styles.textLocation}>{job.location}</div>
-//                            <div className={styles.box}><input type="checkbox" /></div>
-//                            <div className={styles.box}><input type="checkbox" /></div>
-//                            <div className={styles.textDate}>{job.date}</div>
-//                        </div>
-//                    </div>
-//                ))}
-//            </div>
-//        </div>
-//    );
-//}
+export const DescriptionPage = () => {
+    return (
+        <div className={styles['container']}>
+            <div className={styles['header']}>
+                <div className={styles['headerCell']}>{}Company</div>
+                <div className={styles['headerCell']}>{}Favorites</div>
+                <div className={styles['headerCell']}>{}Excluded</div>
+                <div className={styles['headerCell']}>Date</div>
+            </div>
+            <div className={styles['body']}>
+                {dataArray.map(job => (
+                    <div className={styles.grid} key={job.id}>
+                        <div className={styles.row1}>
+                            <div className={styles.textName}>{job.name}</div>
+                        </div>
+                        <div className={styles.row2}>
+                            <div className={styles.textLocation}>{job.location}</div>
+                            <div className={styles.box}><input type="checkbox" /></div>
+                            <div className={styles.box}><input type="checkbox" /></div>
+                            <div className={styles.textDate}>{job.date}</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
