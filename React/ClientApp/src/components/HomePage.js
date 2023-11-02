56ca20a9c5d48661c8e8cbf02ae682c3a5aa9791
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-11-02 10:48:47 +0100

    monthfilterSort

--- React/ClientApp/src/components/HomePage.js
+++ React/ClientApp/src/components/HomePage.js
@@ -21,24 +21,19 @@ export const HomePage = () => {
     },[]);
 
     const [startDate, setStartDate] = useState('');
-    const [endDate, setEndDate] = useState('');
 
     const handleStartDateChange = (e) => {
         setStartDate(e.target.value);
     };
 
-    const handleEndDateChange = (e) => {
-        setEndDate(e.target.value);
-    };
-    const dataArray = [];
     const filterDataByDate = () => {
-
-        dataArray.filter((job) => {
+        const filteredData = serverData.filter((job) => {
             const jobDate = new Date(job.date);
             const filterStartDate = new Date(startDate);
-            const filterEndDate = new Date(endDate);
-            return jobDate >= filterStartDate && jobDate <= filterEndDate;
+            return jobDate >= filterStartDate;
         });
+
+        setServerData(filteredData);
     };
 
     return (
@@ -62,14 +57,9 @@ export const HomePage = () => {
                     </div>
                     <div id={styles.dates}>
                         <input
-                            type="date"
+                            type="month"
                             value={startDate}
                             onChange={handleStartDateChange}
-                        />
-                        <input
-                            type="date"
-                            value={endDate}
-                            onChange={handleEndDateChange}
                         />
                         <button onClick={filterDataByDate}>Date filter</button>
                     </div>
