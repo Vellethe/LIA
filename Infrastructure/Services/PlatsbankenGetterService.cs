08a018d0355322b53babf9b71c9701751d7c1870
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-11-13 09:20:25 +0100

    api can now page thru articles on platsbanken

--- Infrastructure/Services/PlatsbankenGetterService.cs
+++ Infrastructure/Services/PlatsbankenGetterService.cs
@@ -10,10 +10,9 @@ namespace Infrastructure.Services
     {
         public async Task<List<JobScoutJob>> GetData()
         {
-            string data = await GetDataFromApi();
-            PlatsBankenJsonObjectBinding jobs = JsonConvert.DeserializeObject<PlatsBankenJsonObjectBinding>(data);
+            var jobs = await GetDataFromApi();
             List<JobScoutJob> output = new();
-            foreach (var hit in jobs.Hits)
+            foreach (var hit in jobs)
             {
                 output.Add(new JobScoutJob(hit));
             }
@@ -22,31 +21,54 @@ namespace Infrastructure.Services
 
         }
 
-        private async Task<string> GetDataFromApi()
+        private async Task<List<PlatsbankenHit>> GetDataFromApi()
         {
             HttpClient httpClient = new HttpClient();
-            NameValueCollection queryString = System.Web.HttpUtility.ParseQueryString(string.Empty);
+            //turns of some smart search that makes the searching harder
+            httpClient.DefaultRequestHeaders.Add("x-feature-disable-smart-freetext", "true");
+            //only get the feilds we need to reduce network trafic
+            httpClient.DefaultRequestHeaders.Add("X-Fields", "total{value}, hits{id,webpage_url,application_contacts,publication_date, headline, workplace_address{municipality}, employer{name}, description{text}}");
 
-            queryString.Add("q", "python");
-            queryString.Add("region", "zdoY_6u5_Krt");//region code for västa götaland
+            NameValueCollection baseQueryString = System.Web.HttpUtility.ParseQueryString(string.Empty);
 
-            HttpRequestMessage httpRequestMessage = new HttpRequestMessage {
-                RequestUri = new Uri($"https://jobsearch.api.jobtechdev.se/search?{queryString.ToString()}"),
-                Method = HttpMethod.Get,
-                Headers =
+            baseQueryString.Add("q", "python");
+            baseQueryString.Add("region", "zdoY_6u5_Krt");//region code for västa götaland
+            baseQueryString.Add("limit", "100");
+
+
+
+
+            //Note theoretical limit at 2100 jobs per search becouse of platsbanken api limitation
+            var readArticles = 0;
+            var availibleArticles = 2000;
+            List<PlatsbankenHit> output = new List<PlatsbankenHit>();
+
+            while (readArticles < availibleArticles)
+            {
+
+                NameValueCollection currentQueryString = System.Web.HttpUtility.ParseQueryString(string.Empty);
+                currentQueryString.Add("offset", $"{readArticles}");
+
+                //NOTE ToString is required even if it says its not
+                var urlToSend = new Uri($"https://jobsearch.api.jobtechdev.se/search?{baseQueryString.ToString()}&{currentQueryString.ToString()}");
+                HttpRequestMessage httpRequestMessage = new HttpRequestMessage
                 {
-                    {"x-feature-disable-smart-freetext","true" },
-                    //only get the feilds we need to reduce network trafic
-                    {"X-Fields","total{value}, hits{id,webpage_url,application_contacts,publication_date, headline, workplace_address{municipality}, employer{name}, description{text}}" }
-                }
-                
+                    Method = HttpMethod.Get,
+                    RequestUri =  urlToSend 
+                };
 
-            };
+                var respone = await httpClient.SendAsync(httpRequestMessage);
+                var jsonString = await respone.Content.ReadAsStringAsync();
+                //TODO implement network error handeling
+                PlatsBankenJsonObjectBinding jobs = JsonConvert.DeserializeObject<PlatsBankenJsonObjectBinding>(jsonString);
 
-            var respone = await httpClient.SendAsync(httpRequestMessage);
-            var jsonString = await respone.Content.ReadAsStringAsync();
-            //TODO implement network error handeling
-            return jsonString;
+                availibleArticles = jobs.Total.Value;
+                readArticles += 100;
+                output.AddRange(jobs.Hits);
+            }
+
+
+            return output;
         }
     }
 }
