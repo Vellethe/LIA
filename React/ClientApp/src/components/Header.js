c35906bef064c345d7c4eda2d1feabff7157197f
Vellethe <95373802+Vellethe@users.noreply.github.com>
2023-11-17 10:21:18 +0100

    HomeButton works

--- React/ClientApp/src/components/Header.js
+++ React/ClientApp/src/components/Header.js
@@ -4,12 +4,15 @@ import styles from "./Header.module.css"
 import logo from "./../images/consat.png"
 
 export class Header extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            collapsed: false
-        };
-    }
+    //constructor(props) {
+    //    super(props)
+    //    this.state = {
+    //        collapsed: false
+    //    };
+    handleHomeClick = () => {
+        const { history } = this.props;
+        history.push('/');
+    };
 
     render() {
         return (
@@ -17,7 +20,7 @@ export class Header extends Component {
                 <div className={styles.grid}>
                     <img className={styles.brand} alt="ConsatImg" src={logo} />
                     <ul className={styles.links}>
-                        <li className={styles.link}><Link to="/">Home</Link> </li>
+                        <li className={styles.link}><Link to="/" onClick={() => this.handleHomeClick()}>Home</Link> </li>
                         <li className={styles.link}><Link to="/settings">Settings</Link></li>
                         <li className={styles.link}><Link to="/excluded">Excluded</Link></li>
                     </ul>
@@ -31,3 +34,6 @@ export class Header extends Component {
         );
     }
 }
+
+export default (Header);
+
