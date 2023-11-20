2539776d6b068be275e5e99c539f7065ebf8c69b
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-11-20 09:47:33 +0100

    daemon done

--- Daemon/Program.cs
+++ Daemon/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Uri uri = new Uri("https://localhost:7273/api/test");
+            HttpClient httpClient = new HttpClient();
+
+            HttpRequestMessage msg = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = uri
+
+            };
+
+            var response = httpClient.SendAsync(msg);
+            while(response.IsCompleted == false) {
+                Thread.Sleep(500);
+            }
+            Console.WriteLine(response);
         }
     }
 }
